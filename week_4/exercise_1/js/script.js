eedbbc8817e4e56b81d34a17aa157bb0b228c2bf
Vera Lykke Kaasen <55652971+Veralykke@users.noreply.github.com>
2020-02-11 10:28:12 +0100

    Assignment 4

--- week_4/exercise_1/js/script.js
+++ week_4/exercise_1/js/script.js
@@ -1,12 +1,51 @@
-const firstName = document.querySelector("#firstName");
+//CORS
 
+//hvor mange tegn skrives ned:
+
+/*const greeting = "Hello there people";
+const lengthOfGreeting = greeting.length;
+console.log("string chars", lengthOfGreeting);
+const emptyString = "";
+console.log("empty string", emptyString.length);
+
+const spaceTest = "a, b";
+console.log("spaceTest", spaceTest.length);*/
+
+
+
+
+
+//string
+//for å få opp at man skriver inn ...logging..hvor mye, og hva:
+
+/*const firstName = document.querySelector("#firstName");
+console.log(firstName);
 firstName.addEventListener("keyup", logLength);
 
 function logLength(event) {
-  const inputValue = event.target.value;
-  const valueLength = inputValue.length;
-  console.log(valueLength);
+    const inputValue = event.target.value;
+    const valueLength = inputValue.length;
 
-  // we could skip the variables and write it this way
-  console.log(event.target.value.length);
-}
+    console.log(valueLength > 20);
+    if (valueLength > 0) {
+        console.log("the input has a value");
+    } else {
+        console.log("The input does not have a value");
+    }
+}*/
+
+
+
+
+
+//TRIM
+
+/*const firstName = " Vera ";
+console.log(firstName.length);
+
+const trimmedName = firstName.trim();
+console.log(trimmedName.length);
+
+const space = " ";
+console.log(space.length);
+console.log(space.trim().length);*/
